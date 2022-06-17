4072dcf3be2d75c5da7395523533c7ada72d84a6
chatlanin <zimin@chatlanin.com.ru>
2022-06-17 22:13:16 +0300

    t42

--- lua/nvim-tree/renderer/init.lua
+++ lua/nvim-tree/renderer/init.lua
@@ -20,13 +20,13 @@ local M = {
 local namespace_id = api.nvim_create_namespace "NvimTreeHighlights"
 
 local function _draw(bufnr, lines, hl, signs)
-  api.nvim_buf_set_option(bufnr, "modifiable", true)
-  api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-  M.render_hl(bufnr, hl)
-  api.nvim_buf_set_option(bufnr, "modifiable", false)
-  -- for _, sign in pairs(signs) do
-  --   vim.fn.sign_place(0, git.SIGN_GROUP, sign.sign, bufnr, { lnum = sign.lnum, priority = 1 })
-  -- end
+  -- api.nvim_buf_set_option(bufnr, "modifiable", true)
+  -- api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+  -- M.render_hl(bufnr, hl)
+  -- api.nvim_buf_set_option(bufnr, "modifiable", false)
+  for _, sign in pairs(signs) do
+    vim.fn.sign_place(0, git.SIGN_GROUP, sign.sign, bufnr, { lnum = sign.lnum, priority = 1 })
+  end
 end
 
 function M.render_hl(bufnr, hl)
