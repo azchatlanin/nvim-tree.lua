727c3dedf7053f6e350da15abfd74e9fade7aed0
chatlanin <zimin@chatlanin.com.ru>
2022-06-17 22:58:38 +0300

    t58

--- lua/nvim-tree/lib.lua
+++ lua/nvim-tree/lib.lua
@@ -44,10 +44,16 @@ function M.get_last_group_node(node)
 end
 
 function M.expand_or_collapse(node)
-  node.open = not node.open
   if node.has_children then
     node.has_children = false
   end
+
+  if node.open then 
+    node.open = false
+  else
+    node.open = true
+  end
+  --node.open = not node.open
 
   if #node.nodes == 0 then
     core.get_explorer():expand(node)
